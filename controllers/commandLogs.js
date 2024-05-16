ed3f055c8400db2ac6a25e2eb42b99f738fda700
Anshul Vishwakarma <72655193+anssec@users.noreply.github.com>
2024-05-16 20:06:54 +0530

    Update commandLogs.js

--- controllers/commandLogs.js
+++ controllers/commandLogs.js
@@ -1,7 +1,20 @@
-const commandLogs = require("../models/commandLogs");
+const dataBasecommandLogs = require("../models/commandLogs");
 const Response = require("../utils/Response");
+const nodeCache = require("../utils/nodeCash");
 exports.commandGetData = async (req, res) => {
   try {
+    let commandLogs;
+    if (nodeCache.has("commandLogs")) {
+      commandLogs = JSON.parse(nodeCache.get("commandLogs"));
+      Response(res, true, "", 200, commandLogs);
+      return;
+    } else {
+      const user = await dataBasecommandLogs.find({});
+      commandLogs = user;
+      nodeCache.set("commandLogs", JSON.stringify(commandLogs));
+      Response(res, true, "", 200, commandLogs);
+      return;
+    }
     const data = await commandLogs.find();
     // console.log(data);
     return Response(res, true, "", 200, data);
